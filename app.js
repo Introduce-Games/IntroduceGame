f00550667334affc8cb071bdbd2ee464077a98d0
Giyeons <leaf9793@naver.com>
2022-12-16 03:32:00 +0000

    포트번호 변경

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 기본 설정
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 80;
 
 // 정적 파일 불러오기
 app.use(express.static(__dirname + "/public"));
